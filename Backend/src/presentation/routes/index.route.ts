180c7a527e9062e40df4fe94a415f4476bf0d7a5
Moisarm <moisarm03@gmail.com>
2026-01-17 17:23:05 -0400

    [Feature] (Endpoint)  :  added health route to indicate the right working of ther server

--- Backend/src/presentation/routes/index.route.ts
+++ Backend/src/presentation/routes/index.route.ts
@@ -1,6 +1,8 @@
 import express, { type Request, type Response } from "express";
 import { auth_router } from "./auth.route";
+import { health_router } from "./health.route";
 
 export const index_router = express.Router();
 
+index_router.use("/", health_router);
 index_router.use("/auth", auth_router);
