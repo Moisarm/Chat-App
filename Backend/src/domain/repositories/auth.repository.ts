72a9aac8be6fa75cb33291ef4b77bbfabaf194a6
Moisarm <moisarm03@gmail.com>
2026-01-16 22:56:34 -0400

    [Fix] (Domain - Repository) : fixed auth repository, added dtos to functions params and added user_id as a param to update user function

--- Backend/src/domain/repositories/auth.repository.ts
+++ Backend/src/domain/repositories/auth.repository.ts
@@ -1,8 +1,11 @@
+import type {
+  register_dto,
+  update_user_dto,
+} from "../../application/dto/auth.dto";
 import type { User } from "../entities/user.entity";
 
 export interface auth_repository {
-  register(user_data: User): Promise<User>;
-  login(user_data: string): Promise<User>;
-  update_user(user_data: User): Promise<User>;
-  delete_account(user_data: String): Promise<null>;
+  register(user_data: register_dto): Promise<User>;
+  update_user(user_id: string, user_data: update_user_dto): Promise<User>;
+  delete_account(user_id: string): Promise<null>;
 }
