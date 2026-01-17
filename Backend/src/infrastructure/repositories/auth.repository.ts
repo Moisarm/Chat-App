3d9234e26e4a5190be001dd4abce6f2e0abb07c8
Moisarm <moisarm03@gmail.com>
2026-01-16 23:36:15 -0400

    [Feature] (Infrastructure - Repository) :  auth repository implementation on infrastructure layer

---
+++ Backend/src/infrastructure/repositories/auth.repository.ts
@@ -0,0 +1,48 @@
+import type {
+  register_dto,
+  update_user_dto,
+} from "../../application/dto/auth.dto";
+import type { User } from "../../domain/entities/user.entity";
+import type { auth_repository } from "../../domain/repositories/auth.repository";
+import { prisma } from "../config/database/prisma.config";
+
+export class auth_repository_implemented implements auth_repository {
+  async register(user_data: register_dto): Promise<User> {
+    const new_user = await prisma.user.create({
+      data: {
+        ...user_data,
+      },
+    });
+
+    return new_user as User;
+  }
+
+  async update_user(
+    user_id: string,
+    user_data: update_user_dto,
+  ): Promise<User> {
+    const now = new Date();
+
+    const user = {
+      ...user_data,
+      updated_at: now,
+    };
+
+    const updated_user = await prisma.user.update({
+      where: { id: user_id },
+      data: user,
+    });
+
+    return updated_user as User;
+  }
+
+  async delete_account(user_data: string): Promise<null> {
+    await prisma.user.delete({
+      where: {
+        email: user_data,
+      },
+    });
+
+    return null;
+  }
+}
