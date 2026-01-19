80d59dee33313c471398f9a9772a0a68fbad66c1
Moisarm <moisarm03@gmail.com>
2026-01-18 23:23:41 -0400

    [Feature] (Auth - Route) : imported in the auth route, the login and register validation classes, and validation-dto middleware,  added the use of the middleware in the login and register routes and  the send of cookies in register and the maxAge attribute for the cookies

--- Backend/src/presentation/routes/auth.route.ts
+++ Backend/src/presentation/routes/auth.route.ts
@@ -8,6 +8,11 @@ import type {
   register_dto,
   user_login_dto,
 } from "../../application/dto/auth.dto";
+import { validate_class } from "../middlewares/validate-dto.middleware";
+import {
+  register_validation,
+  user_login_validation,
+} from "../../infrastructure/external/validation/auth.validation";
 
 const auth_router = express.Router();
 
@@ -15,12 +20,20 @@ const controller = new auth_controller();
 
 auth_router.post(
   "/register",
+  validate_class(register_validation),
   async (req: Request, res: Response, next: NextFunction) => {
     const user_data: register_dto = req.body;
 
     console.log("Body: ", user_data);
     try {
       const response = await controller.register(user_data);
+
+      if (response.data?.token) {
+        res.cookie("Access-Token", response.data.token, {
+          httpOnly: true,
+          maxAge: 60 * 60 * 1000,
+        });
+      }
       res.status(response.status).json(response);
     } catch (error) {
       next(error);
@@ -30,6 +43,7 @@ auth_router.post(
 
 auth_router.post(
   "/login",
+  validate_class(user_login_validation),
   async (req: Request, res: Response, next: NextFunction) => {
     const login_data: user_login_dto = req.body;
 
@@ -39,6 +53,7 @@ auth_router.post(
       if (response.data?.token) {
         res.cookie("Access-Token", response.data.token, {
           httpOnly: true,
+          maxAge: 60 * 60 * 1000,
         });
       }
 
