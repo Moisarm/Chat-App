1114bd50c6d8a99e8d3ecf04fcef6d471ecaa749
Moisarm <moisarm03@gmail.com>
2026-01-21 04:42:24 -0400

    [Fix] (Repository) : modified the import of user repository in register use case due to an change in the class name

--- Backend/src/application/use-cases/auth/register.use-case.ts
+++ Backend/src/application/use-cases/auth/register.use-case.ts
@@ -1,5 +1,5 @@
 import type { auth_repository } from "../../../domain/repositories/auth.repository";
-import type { User_repository } from "../../../domain/repositories/user.repository";
+import type { user_repository } from "../../../domain/repositories/user.repository";
 import { failure, success } from "../../../domain/result/result-pattern";
 import type { register_dto } from "../../dto/auth.dto";
 import { generate_token_service } from "../../services/auth/create-token.service";
@@ -7,13 +7,13 @@ import { hash_password_service } from "../../services/auth/hash-password.service
 
 export class register_use_case {
   private readonly auth_repository: auth_repository;
-  private readonly user_repository: User_repository;
+  private readonly user_repository: user_repository;
   private readonly generate_token_service: generate_token_service;
   private readonly hash_password_service: hash_password_service;
 
   constructor(
     auth_repository: auth_repository,
-    user_repository: User_repository,
+    user_repository: user_repository,
   ) {
     this.auth_repository = auth_repository;
     this.user_repository = user_repository;
