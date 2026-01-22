76db02e009d6712994bbab42d74d021dec73a50f
Moisarm <moisarm03@gmail.com>
2026-01-21 20:20:46 -0400

    [Feature] (Domain - Repository) : Added find by id function to domain user repository

--- Backend/src/domain/repositories/user.repository.ts
+++ Backend/src/domain/repositories/user.repository.ts
@@ -3,5 +3,6 @@ import type { User } from "../entities/user.entity";
 export interface user_repository {
   find_by_email(email: string): Promise<User>;
   find_by_username(username: string): Promise<User>;
+  find_by_id(id: string): Promise<User>;
   get_users(): Promise<User[]>;
 }
