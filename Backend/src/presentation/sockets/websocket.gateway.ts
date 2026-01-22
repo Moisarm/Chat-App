6d7d4f2adc819eed350ce692ef6902e718756fa4
Moisarm <moisarm03@gmail.com>
2026-01-22 01:32:05 -0400

    [Feature] (Socket) : Server started listening to message event

--- Backend/src/presentation/sockets/websocket.gateway.ts
+++ Backend/src/presentation/sockets/websocket.gateway.ts
@@ -7,8 +7,17 @@ export class websocket_gateway {
     console.log(`An user has connected id: ${socket.id}`);
 
     /*here i can manage the events */
-    socket.on("disconnect", () => console.log("An user has disconnected"));
+    socket.on("message", (message_content: string) => {
+      this.on_message(socket, message_content);
+    });
+
+    socket.on("disconnect", () =>
+      console.log(`An user has disconnected id:  ${socket.id}`),
+    );
   }
 
+  private on_message(socket: Socket, message_content: string) {
+    console.log(`Message received from ${socket.id}: ${message_content}`);
+  }
   /*Here should be controllers */
 }
