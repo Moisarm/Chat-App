52a5951bc3e36547e438c95344376394373d9753
Moisarm <moisarm03@gmail.com>
2026-01-22 01:04:55 -0400

    [Fix] (Use-Case) : Hashing the password if its provided and comparing it to the old password to dont replace it if its the same

--- Backend/src/application/use-cases/auth/update-user.use-case.ts
+++ Backend/src/application/use-cases/auth/update-user.use-case.ts
@@ -2,12 +2,16 @@ import type { auth_repository } from "../../../domain/repositories/auth.reposito
 import type { user_repository } from "../../../domain/repositories/user.repository";
 import { failure, success } from "../../../domain/result/result-pattern";
 import type { update_user_dto } from "../../dto/auth.dto";
+import { compare_password_service } from "../../services/auth/compare-passwords.service";
 import { decode_token_service } from "../../services/auth/decode-token.service";
+import { hash_password_service } from "../../services/auth/hash-password.service";
 
 export class update_user_use_case {
   private readonly auth_repository: auth_repository;
   private readonly user_repository: user_repository;
   private readonly decode_token_service: decode_token_service;
+  private readonly hash_password_service: hash_password_service;
+  private readonly compare_password_service: compare_password_service;
 
   constructor(
     auth_repository: auth_repository,
@@ -16,6 +20,8 @@ export class update_user_use_case {
     this.auth_repository = auth_repository;
     this.user_repository = user_repository;
     this.decode_token_service = new decode_token_service();
+    this.hash_password_service = new hash_password_service();
+    this.compare_password_service = new compare_password_service();
   }
 
   async run(token: string, update_user_data: update_user_dto) {
@@ -29,6 +35,33 @@ export class update_user_use_case {
           error: "User not found",
         };
         return failure(fail);
+      }
+      if (update_user_data.password) {
+        const user_password = await this.compare_password_service.run(
+          update_user_data.password,
+          user.password,
+        );
+
+        if (user_password.succes) {
+          let fail = {
+            status: 400,
+            message: "New password cannot be the same as the old password",
+            error: "Bad Request",
+          };
+
+          return failure(fail);
+        }
+
+        const updated_password = await this.hash_password_service.run(
+          update_user_data.password,
+        );
+
+        if (updated_password.succes) {
+          update_user_data = {
+            ...update_user_data,
+            password: updated_password.data,
+          };
+        }
       }
       const updated_user = await this.auth_repository.update_user(
         decoded_token.user_id,
