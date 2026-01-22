2b19cb56d2caffe974626660f3c8a7120ef89a51
Moisarm <moisarm03@gmail.com>
2026-01-21 21:12:44 -0400

    [Feature] (Route) : Added user route to index router

--- Backend/src/presentation/routes/index.route.ts
+++ Backend/src/presentation/routes/index.route.ts
@@ -1,8 +1,10 @@
 import express, { type Request, type Response } from "express";
 import { auth_router } from "./auth.route";
 import { health_router } from "./health.route";
+import { users_router } from "./users.route";
 
 export const index_router = express.Router();
 
 index_router.use("/", health_router);
 index_router.use("/auth", auth_router);
+index_router.use("/users", users_router);
