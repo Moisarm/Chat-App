ac86ae5819ab00051835400dfa0505883434f98a
Moisarm <moisarm03@gmail.com>
2026-01-23 02:37:05 -0400

    [Feature] (Domain - Repository) : added a fuction to update the status as admin in a group and commented get chat participants due to the handle of this situation in chat's repo

--- Backend/src/domain/repositories/participant.repository.ts
+++ Backend/src/domain/repositories/participant.repository.ts
@@ -2,6 +2,11 @@ import type { Participant } from "../entities/participant.entity";
 
 export interface participant_repository {
   add_participant(user_id: string, chat_id: string): Promise<Participant>;
-  get_chat_participants(chat_id: string): Promise<Participant[]>;
-  delete_participant(user_id: string, chat_id: string): Promise<void>;
+  // get_chat_participants(chat_id: string): Promise<Participant[]>;
+  delete_participant(chat_id: string, user_id: string): Promise<void>;
+  update_admin_status(
+    chat_id: string,
+    user_id: string,
+    status: boolean,
+  ): Promise<void>;
 }
