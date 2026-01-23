a4420e06d3d2168626655bd8874c9d5a040cf3db
Moisarm <moisarm03@gmail.com>
2026-01-23 02:27:31 -0400

    [Feature] (Route) : Added chat route to index router

--- Backend/src/presentation/routes/index.route.ts
+++ Backend/src/presentation/routes/index.route.ts
@@ -2,9 +2,11 @@ import express, { type Request, type Response } from "express";
 import { auth_router } from "./auth.route";
 import { health_router } from "./health.route";
 import { users_router } from "./users.route";
+import { chat_router } from "./chat.route";
 
 export const index_router = express.Router();
 
 index_router.use("/", health_router);
 index_router.use("/auth", auth_router);
 index_router.use("/users", users_router);
+index_router.use("/chat", chat_router);
