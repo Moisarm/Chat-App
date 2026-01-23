cc38a9004aedfba66cfd27c8ad5e58ca0378aded
Moisarm <moisarm03@gmail.com>
2026-01-23 02:24:18 -0400

    [Feature] (Use-Case) : Created get user chats use case, an use case that obtain all user's chat from their id pass by token

---
+++ Backend/src/application/use-cases/chat/get-users-chats.use-case.ts
@@ -0,0 +1,30 @@
+import { decode } from "jsonwebtoken";
+import type { chat_repository } from "../../../domain/repositories/chat.repository";
+import { decode_token_service } from "../../services/auth/decode-token.service";
+import { failure, success } from "../../../domain/result/result-pattern";
+
+export class get_user_chat_use_case {
+  private readonly chat_repository: chat_repository;
+  private readonly decode_token_service: decode_token_service;
+
+  constructor(chat_repository: chat_repository) {
+    this.chat_repository = chat_repository;
+    this.decode_token_service = new decode_token_service();
+  }
+
+  async run(token: string) {
+    const decoded = this.decode_token_service.run(token);
+    const chats = await this.chat_repository.get_user_chat(decoded.user_id);
+
+    if (chats.length == 0) {
+      let fail = {
+        status: 200,
+        message: "No chats yet",
+        error: "Empty table",
+      };
+      return failure(fail);
+    }
+
+    return success(chats);
+  }
+}
