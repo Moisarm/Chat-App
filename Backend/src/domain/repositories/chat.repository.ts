7a64ab101e8461b0f31e3c9776cf04c496f3b2d7
Moisarm <moisarm03@gmail.com>
2026-01-23 02:20:35 -0400

    [Feature] (Domain - Repository) : Fixed and added functions to chat repository

--- Backend/src/domain/repositories/chat.repository.ts
+++ Backend/src/domain/repositories/chat.repository.ts
@@ -1,8 +1,14 @@
+import type {
+  create_chat_dto,
+  create_group_dto,
+} from "../../application/dto/chat.dto";
 import type { Chat } from "../entities/chat.entity";
 
 export interface chat_repository {
   find_chat_by_id(chat_id: string): Promise<Chat>;
-  create_chat(is_group: boolean, username: string[]): Promise<Chat>;
+  find_private_chat(user_id: string, target_user_id: string): Promise<Chat>;
+  create_chat(current_user_id: string, data: create_chat_dto): Promise<Chat>;
+  create_group(current_user_id: string, data: create_group_dto): Promise<Chat>;
   get_user_chat(user_id: string): Promise<Chat[]>;
   delete_chat(chat_id: string): Promise<null>;
 }
