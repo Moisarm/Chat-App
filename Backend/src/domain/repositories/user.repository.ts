53cffecfa56ff303dcaf57a48f9a8e041f567f41
Moisarm <moisarm03@gmail.com>
2026-01-26 03:23:05 -0400

    [Feature] (Domain - Repository) :
    added find many by id function in the user domain repository interface

--- Backend/src/domain/repositories/user.repository.ts
+++ Backend/src/domain/repositories/user.repository.ts
@@ -4,5 +4,6 @@ export interface user_repository {
   find_by_email(email: string): Promise<User>;
   find_by_username(username: string): Promise<User>;
   find_by_id(id: string): Promise<User>;
+  find_many_by_id(users_id: string[]): Promise<User[]>;
   get_users(): Promise<User[]>;
 }
