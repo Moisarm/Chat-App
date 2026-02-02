8f8e9343ef6b4bc0615670826b2d36ded608099b
Moisarm <moisarm03@gmail.com>
2026-02-02 02:46:00 -0400

    [Feature] (Sockets) :
    Created the event new group, wich creates a new group using a group name and users id's.
    Fixed the gateway structure, added a try catch to get te  cookie and disallow to use those functions to people not authtenticated.
    
    Note:
    Theres a bug on new group, the persistence of the chat is successfull, but theres a fail in the socket, I must fix this asap

--- Backend/src/presentation/sockets/websocket.gateway.ts
+++ Backend/src/presentation/sockets/websocket.gateway.ts
@@ -1,13 +1,43 @@
 import type { Server, Socket } from "socket.io";
+
 import type { new_message_dto } from "../../application/dto/message.dto";
 
+import type { create_group_dto } from "../../application/dto/chat.dto";
+
+import type { create_group_use_case } from "../../application/use-cases/chat/create-group.use-case";
+
+import { decode_token_service } from "../../application/services/auth/decode-token.service";
+import { failure } from "../../domain/result/result-pattern";
+
 export class websocket_gateway {
-  constructor() {}
+  private readonly decode_token_service: decode_token_service;
+
+  constructor(private create_group_use_case: create_group_use_case) {
+    this.decode_token_service = new decode_token_service();
+  }
 
   public on_connection(io: Server, socket: Socket) {
     console.log(`An user has connected id: ${socket.id}`);
+    const token = socket.request.headers.cookie;
 
-    /*here i can manage the events */
+    try {
+      if (!token) {
+        console.log("Invalid Token");
+        return;
+      }
+      const decoded = this.decode_token_service.run(token);
+      socket.data.user_id = decoded.user_id;
+      socket.join(`user:${socket.data.user_id}`);
+      console.log(
+        `User ${socket.data.user_id} authenticated and joined private room.`,
+      );
+    } catch (error) {
+      socket.emit("error", { message: "Invalid Token", code: 401 });
+      return;
+    }
+
+    /*Events */
+
     socket.on("join_chat", async (chat_id) => {
       socket.join(chat_id);
     });
@@ -16,17 +46,50 @@ export class websocket_gateway {
       this.on_new_message(io, socket, data);
     });
 
+    socket.on("new_group", async (data: create_group_dto) => {
+      this.on_new_group(io, socket, token, data);
+    });
+
     socket.on("disconnect", () =>
-      console.log(`An user has disconnected id:  ${socket.id}`),
+      console.log(`An user has disconnected id: ${socket.id}`),
     );
   }
 
-  private on_new_message(io: Server, socket: Socket, data: new_message_dto) {
-    console.log(`Message received from ${socket.id}: ${data.content}`);
+  /*Controllers */
+  private async on_new_group(
+    io: Server,
 
-    //save on db
-    const saved_message = "Test"; //Database response
-    io.to(data.chat_id).emit("new_message", saved_message);
+    socket: Socket,
+
+    token: string,
+
+    data: create_group_dto,
+  ) {
+    console.log(`Group creation request from ${socket.id}: ${data.group_name}`);
+
+    const result = await this.create_group_use_case.run(token, data);
+
+    if (!result.succes) {
+      return socket.emit("error", result.error);
+    }
+
+    const new_group = result.data;
+
+    new_group.data.participant.forEach((participant) => {
+      const user_id = participant.id;
+      io.to(`user:${user_id}`).emit("added_to_group", {
+        message: `You have been added to ${new_group.data.group_name}`,
+        group: new_group,
+      });
+    });
+
+    console.log(new_group);
   }
-  /*Here should be controllers */
+
+  private on_new_message(io: Server, socket: Socket, data: new_message_dto) {
+    io.to(data.chat_id).emit("new_message", {
+      content: data.content,
+      sender: socket.data.user_id,
+    });
+  }
 }
--- Backend/src/presentation/sockets/websocket.module.ts
+++ Backend/src/presentation/sockets/websocket.module.ts
@@ -1,13 +1,23 @@
 import { Server as http_server } from "node:http";
 import { Server as socket_server } from "socket.io";
 import { websocket_gateway } from "./websocket.gateway";
+import { create_group_use_case } from "../../application/use-cases/chat/create-group.use-case";
+import { user_repository_implemented } from "../../infrastructure/repositories/user.repository";
+import { chat_repository_implemented } from "../../infrastructure/repositories/chat.repository";
 export class socket_module {
   public static init(http_server: http_server) {
     const io = new socket_server(http_server, {
       cors: { origin: "*" },
       connectionStateRecovery: {},
     });
-    const controller = new websocket_gateway();
+
+    const user_repository = new user_repository_implemented();
+    const chat_repository = new chat_repository_implemented();
+    const new_group_use_case = new create_group_use_case(
+      user_repository,
+      chat_repository,
+    );
+    const controller = new websocket_gateway(new_group_use_case);
 
     io.on("connection", (socket) => {
       controller.on_connection(io, socket);
