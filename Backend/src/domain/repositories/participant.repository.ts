72ba1052a41a85c974acd431d72b7b283dc0d32a
Moisarm <moisarm03@gmail.com>
2026-02-25 01:24:47 -0400

    [Fix] (Repository) : added null as option in the response of find participant function

--- Backend/src/domain/repositories/participant.repository.ts
+++ Backend/src/domain/repositories/participant.repository.ts
@@ -6,7 +6,10 @@ export interface participant_repository {
     chat_id: string,
   ): Promise<{ count: number }>;
   // get_chat_participants(chat_id: string): Promise<Participant[]>;
-  find_participant(chat_id: string, user_id: string): Promise<Participant>;
+  find_participant(
+    chat_id: string,
+    user_id: string,
+  ): Promise<Participant | null>;
   delete_participant(chat_id: string, users_id: string[]): Promise<void>;
   update_admin_status(participant: Participant): Promise<Participant>;
 }
